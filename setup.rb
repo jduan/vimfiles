2dce9cab3e0c6287cf6c70bf945f2e9d30fa6046
Jingjing Duan <jingjing.duan@hulu.com>
2012-06-01 15:28:41 -0700

    update setup.rb

--- setup.rb
+++ setup.rb
@@ -5,16 +5,17 @@
 
 require "fileutils"
 
+EXCLUDE_LIST = [ ".", "..", ".git" ]
+
 target_dir = Dir.home + "/vimfiles/"
 src_dir = Dir.home + "/"
 Dir.chdir(target_dir)
 Dir.glob('.*').each do |file|
-  if (file != "." && file != ".." && file != ".git")
+  unless EXCLUDE_LIST.include? file
     src_path = src_dir + file
     target_path = target_dir + file
     if File.exists?(src_path)
       FileUtils.rm_rf(src_path)
-      puts "#{src_path} exists already, just deleted"
     end
     puts "create symlink: #{src_path}"
     `ln -s #{target_path} #{src_path}`
